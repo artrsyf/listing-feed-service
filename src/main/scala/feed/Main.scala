1171e781a94e39fb6378f2d52cb8f0c0b0d308cc
Фадеев Роман Андреевич <ro.fadeev@tbank.ru>
2026-04-21 14:20:07 +0300

    Add swagger endpoint

--- src/main/scala/feed/Main.scala
+++ src/main/scala/feed/Main.scala
@@ -1,6 +1,9 @@
 package feed
 
+import sttp.apispec.openapi.Info
 import sttp.tapir.server.ziohttp._
+import sttp.tapir.swagger.SwaggerUIOptions
+import sttp.tapir.swagger.bundle.SwaggerInterpreter
 import zio._
 import zio.config.typesafe._
 import zio.http.Server
@@ -8,7 +11,6 @@ import zio.http.Server
 import feed.listing.delivery.ListingHttpHandler
 import feed.listing.repository.ListingPostgresRepository
 import feed.listing.route.ListingRouteImpl
-import feed.listing.shared.infrastructure.DbConfig
 import feed.listing.shared.infrastructure.PostgresDataSource
 import feed.listing.usecase.ListingService
 
@@ -25,9 +27,17 @@ object Main extends ZIOAppDefault {
       aggregator <- ZIO
         .service[RouteAggregator]
 
+      routes = aggregator.allRoutes.toList
+
+      swaggerEndpoints =
+        SwaggerInterpreter(swaggerUIOptions =
+          SwaggerUIOptions.default.copy(pathPrefix = List("api", "feed", "docs"))
+        )
+          .fromEndpoints[Task](routes.map(_.endpoint), Info("Feed API", "1.0"))
+
       app =
         ZioHttpInterpreter()
-          .toHttp(aggregator.allRoutes.toList)
+          .toHttp(routes ++ swaggerEndpoints)
 
       _ <- Server.serve(app)
     } yield ()).provide(
